94e4df92911629a7a66a08c9b9b075918dac326a
Huynh Ngoc Hau <ngochau1310@gmail.com>
2025-09-06 10:17:03 +0700

    order and logActivity

---
+++ ASA-PLATFORM-BE/ASA-PLATFORM-BE/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using ASA_PLATFORM_REPO.Models;
+using ASA_PLATFORM_SERVICE.DTOs.Request;
+using ASA_PLATFORM_SERVICE.DTOs.Response;
+using ASA_PLATFORM_SERVICE.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASA_PLATFORM_BE.Controllers
+{
+    [Route("api/Orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetFiltered([FromQuery]OrderGetRequest requestDto, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var result = await _orderService.GetFilteredProductsAsync(requestDto, page, pageSize);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<OrderResponse>> Create([FromBody] OrderRequest request)
+        {
+            var result = await _orderService.CreateAsync(request);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<OrderResponse>> Update(long id, [FromBody] OrderRequest request)
+        {
+            var result = await _orderService.UpdateAsync(id, request);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(long id)
+        {
+            var result = await _orderService.DeleteAsync(id);
+            return Ok(result);
+        }
+    }
+}
--- ASA-PLATFORM-BE/ASA-PLATFORM-REPO/Repository/OrderRepo.cs
+++ ASA-PLATFORM-BE/ASA-PLATFORM-REPO/Repository/OrderRepo.cs
@@ -14,5 +14,35 @@ namespace ASA_PLATFORM_REPO.Repository
         public OrderRepo(ASAPLATFORMDBContext context) : base(context)
         {
         }
+        public IQueryable<Order> GetFiltered(Order filter)
+        {
+            var query = _context.Orders.AsQueryable();
+
+            if (filter.OrderId > 0)
+                query = query.Where(c => c.OrderId == filter.OrderId);
+            if (!string.IsNullOrEmpty(filter.Note))
+                query = query.Where(c => c.Note.Contains(filter.Note));
+            if (filter.TotalPrice.HasValue)
+                query = query.Where(p => p.TotalPrice <= filter.TotalPrice);
+            if (filter.CreatedAt.HasValue)
+                query = query.Where(p => p.CreatedAt <= filter.CreatedAt);
+            if (filter.ExpiredAt.HasValue)
+                query = query.Where(p => p.ExpiredAt <= filter.ExpiredAt);
+            if (filter.Status.HasValue)
+                query = query.Where(p => p.Status == filter.Status);
+            if(filter.Discount.HasValue)
+                query = query.Where(p => p.Discount <= filter.Discount);
+            if(filter.PaymentMethod != null)
+                query = query.Where(p => p.PaymentMethod == filter.PaymentMethod);
+            if (filter.UserId > 0)
+                query = query.Where(p => p.UserId == filter.UserId);
+            if (filter.ShopId > 0)
+                query = query.Where(p => p.ShopId == filter.ShopId);
+            if (filter.ProductId > 0)
+                query = query.Where(p => p.ProductId == filter.ProductId);
+
+
+            return query.OrderBy(c => c.OrderId);
+        }
     }
 }
