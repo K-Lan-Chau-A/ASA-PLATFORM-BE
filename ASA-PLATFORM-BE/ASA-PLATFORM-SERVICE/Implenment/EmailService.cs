655aa4210348612a0f3cc3b0c1868306c59fc278
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-10-03 10:21:05 +0700

    email service

---
+++ ASA-PLATFORM-BE/ASA-PLATFORM-SERVICE/Implenment/EmailService.cs
@@ -0,0 +1,46 @@
+﻿using ASA_PLATFORM_SERVICE.Interface;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace ASA_PLATFORM_SERVICE.Implenment
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("ASA Platform", _config["SmtpSettings:Username"]));
+            message.To.Add(MailboxAddress.Parse(to));
+            message.Subject = subject;
+            message.Body = new TextPart("html")
+            {
+                Text = body
+            };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(
+                _config["SmtpSettings:Host"],
+                int.Parse(_config["SmtpSettings:Port"]),
+                SecureSocketOptions.StartTls
+            );
+
+            await client.AuthenticateAsync(
+                _config["SmtpSettings:Username"],
+                _config["SmtpSettings:Password"]
+            );
+
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
