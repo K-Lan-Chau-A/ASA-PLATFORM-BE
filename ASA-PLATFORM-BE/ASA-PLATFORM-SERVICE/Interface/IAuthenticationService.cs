609b2c5ff3320c547426582385deb3b9ec73a587
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-10-13 16:15:55 +0700

    features claim

--- ASA-PLATFORM-BE/ASA-PLATFORM-SERVICE/Interface/IAuthenticationService.cs
+++ ASA-PLATFORM-BE/ASA-PLATFORM-SERVICE/Interface/IAuthenticationService.cs
@@ -13,7 +13,7 @@ namespace ASA_PLATFORM_SERVICE.Interface
     public interface IAuthenticationService
     {
         public Task<ApiResponse<LoginResponse>> Login(LoginRequest loginRequest);
-        public Task<string> GenerateToken(User account);
+        public Task<string> GenerateToken(User account, List<long>? featureIds = null);
         public Task<ApiResponse<CurrentAccount>> GetCurrentAccount(string username);
         Task<ApiResponse<ValidateShopResponse>> ValidateShop(ValidateTenantLoginRequest dto);
     }
